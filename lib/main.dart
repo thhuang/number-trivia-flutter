3d2ed39b56880a2afc0bf4fec5e52f228d9e7bf1
TH Huang <tzhsn.huang@gmail.com>
2019-11-07 16:40:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(NumberTrivia());
+
+class NumberTrivia extends StatelessWidget {
+  const NumberTrivia({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
